0fbcafff45457265f589d2953d032341cb46ecea
YellowHIT <109045292+YellowHIT@users.noreply.github.com>
2022-07-13 23:58:44 -0300

    Second Commit buttons
    Transition between scenes and buttons working

---
+++ Assets/Scripts/MenuUiHandler.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUiHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+     public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Exit()
+    {
+        #if UNITY_EDITOR
+
+            EditorApplication.ExitPlaymode();
+    
+        #else
+        
+            Application.Quit();
+
+        #endif
+    
+    }
+
+}
